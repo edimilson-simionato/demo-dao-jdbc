6af7040b3820dea7171157e80bd4413f446d44fb
Edimilson Gatti Simionato <edimilson@uol.com.br>
2020-10-21 16:42:53 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department dpt = new Department(1, "Contabilidade");
 		
-		System.out.println(dpt);
+		Seller seller = new Seller(1, "Peter Frampton", "p_frampton@babaquice.com", new Date(), 3500.0, dpt);
+		
+		System.out.println(seller);
 	}
 
 }
